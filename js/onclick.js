79213ebb69eeee35e8c69b12cbf4a34d94b54712
jose alisson bezerra <josealissonbezerra@hotmail.com>
2023-10-04 16:32:57 -0300

    feat: ajustando arquivos antigos

--- js/onclick.js
+++ js/onclick.js
@@ -1,32 +1,29 @@
 
 function abrecaderno(){
-
     document.getElementById("left").style = "transition: 0.7s ease-in-out;transform: rotateY(0deg)!important; ";
     document.getElementById("right").style = "transition: 1.5s ease-in-out;transform: rotateY(0deg)!important;";
 
     setTimeout(() => {
         document.getElementById("front").style = "z-index:4";
-    }, 0500);
+    }, 500);
 
     setTimeout(() => {
         document.getElementById("backr").style = "display:block";
         document.getElementById("close").style = "transition: 1.5s ease-in-out;display:block";
-    }, 0800);
+    }, 800);
 
 }
 
 function fechacaderno(){
-
     document.getElementById("left").style = "transition: 1.5s ease-in-out; z-index: 20; transform: rotateY(180deg); transform-origin: 100% 50%;";
     document.getElementById("right").style = "transition: 0.7s ease-in-out;transform: rotateY(-180deg)!important;";
 
     setTimeout(() => {
         document.getElementById("front").style = "z-index:5";
-    }, 0800);
+    }, 800);
 
     setTimeout(() => {
         document.getElementById("backr").style = "display:none";
         document.getElementById("close").style = "transition: 1.5s ease-in-out;display:none";
-    }, 0500);
-
+    }, 500);
 }
